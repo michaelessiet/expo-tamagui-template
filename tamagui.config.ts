09723abc534f4ab63eb4e23574807556c7de2b75
michaelessiet <emsaa2002@gmail.com>
2023-10-19 11:37:40 +0100

    chore: added the tamagui ugrade commands and wrote a readme

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,14 +1,15 @@
 import { createTamagui } from "tamagui"
-import { config } from "@tamagui/config/v2"
+import { config } from "@tamagui/config/v2-native"
 
 const tamaguiConfig = createTamagui(config)
-// this makes typescript properly type everything based on the config
 
+// this makes typescript properly type everything based on the config
 type Conf = typeof tamaguiConfig
 
 declare module "tamagui" {
 	interface TamaguiCustomConfig extends Conf {}
 }
+
 export default tamaguiConfig
 // depending on if you chose tamagui, @tamagui/core, or @tamagui/web
 
